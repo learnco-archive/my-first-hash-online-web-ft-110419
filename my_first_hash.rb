c596d23fa5e4555169d5ce29c6d3bca8c91ce28e
Jess Fischbach <robotspacefish@gmail.com>
2019-11-08 01:42:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,10 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {
-  "name" => "Jess",
-  "occupation" => "Software Engineer"
-}
-
+  my_hash = {
+    "name" => "Jess",
+    "occupation" => "Software Engineer"
+  }
 end
 
 
@@ -17,7 +16,6 @@ def shipping_manifest
     "porcelain vases" => 2,
     "oil paintings" => 3
   }
-
 end
 
 def retrieval
@@ -28,7 +26,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
